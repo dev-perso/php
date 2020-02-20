e35d53879ef265af2dc8fa942030f3b7636cf54f
MParisNRX <michael.paris@nrx.eu>
2020-02-20 17:58:38 +0100

    evol

--- MaCaveAVin/public/js/macave.js
+++ MaCaveAVin/public/js/macave.js
@@ -1,33 +1,12 @@
 document.addEventListener("DOMContentLoaded", function()
 {
-    var tr      = document.getElementsByTagName("tr");
-    var use     = document.getElementsByClassName("useWine");
-    var edit    = document.getElementsByClassName("editWine");
-    var filter  = document.getElementsByClassName("btnFilter");
-    var request = new XMLHttpRequest();
-
-    for (var i = 0; i < use.length; i++)
-    {
-        use[i].addEventListener("click", useWine, false);
-    }
-
-    for (var i = 0; i < edit.length; i++)
-    {
-        edit[i].addEventListener("click", editWine, false);
-    }
-    
-    for (var i = 0; i < filter.length; i++)
-    {
-        filter[i].addEventListener("click", filterMyCave, false);
-    }
-
-    for (var i = 0; i < tr.length; i++)
-    {
-        for (var j = 0; j < (tr[i].cells.length - 1); j++)
-        {
-            tr[i].cells[j].addEventListener("click", getDescription, false);
-        }
-    }
+    var macave      = document.getElementById("macave");
+    var actifFilter = document.getElementById("actifFilter");
+    var tr          = document.getElementsByTagName("tr");
+    var use         = document.getElementsByClassName("useWine");
+    var edit        = document.getElementsByClassName("editWine");
+    var filter      = document.getElementsByClassName("btnFilter");
+    var request     = new XMLHttpRequest();
 
     function editWine()
     {
@@ -58,25 +37,35 @@ document.addEventListener("DOMContentLoaded", function()
     
     function filterMyCave()
     {
-        var constraint = this.getAttribute("data-filter");
-        alert(constraint);
+        var constraint  = this.getAttribute("data-filter");
+        var url         = "/caveavin/filtre/" + constraint;
 
-        /*request.open('POST', url, true);
+        request.open('POST', url, true);
         request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        request.withCredentials = true;
-        request.send(paramsReq);
-    
-        request.onload = function()
-        {
-    
-        };
-    
+        request.send();
+
         request.onreadystatechange = function()
         {
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
             {
+                const response      = JSON.parse(request.response);
+                var buttonFilter    = document.createElement('button');
+                var span            = document.createElement('span');
+
+                // Création button du filtre actif
+                span.innerText = response['filtres'];
+                buttonFilter.classList.add('actif-filtre');
+                buttonFilter.appendChild(span);
+                actifFilter.appendChild(buttonFilter);
+
                 
+                console.log(buttonFilter);
+
+
+
+                console.log(JSON.parse(request.response));
+
             }
         };
     
@@ -88,6 +77,38 @@ document.addEventListener("DOMContentLoaded", function()
         request.onprogress = function(event)
         {
             //console.log("Reçu " + event.loaded + " sur " + event.total);
-        };*/
+        };
+    }
+
+    /**
+     * AddEventListener
+     *
+     * click sur les boutons Archive
+     * click sur les boutons Edition
+     * click sur les boutons Filtre
+     * click sur les lignes du tableau pour Description
+     */
+
+    for (var i = 0; i < use.length; i++)
+    {
+        use[i].addEventListener("click", useWine, false);
+    }
+
+    for (var i = 0; i < edit.length; i++)
+    {
+        edit[i].addEventListener("click", editWine, false);
+    }
+
+    for (var i = 0; i < filter.length; i++)
+    {
+        filter[i].addEventListener("click", filterMyCave, false);
+    }
+
+    for (var i = 0; i < tr.length; i++)
+    {
+        for (var j = 0; j < (tr[i].cells.length - 1); j++)
+        {
+            tr[i].cells[j].addEventListener("click", getDescription, false);
+        }
     }
 });
--- MaCaveAVin/src/Controller/CaveAVinController.php
+++ MaCaveAVin/src/Controller/CaveAVinController.php
@@ -27,6 +27,11 @@ class CaveAVinController extends AbstractController
      */
     private $couleur;
 
+    /*
+     * @var Array
+     */
+    private $region;
+
     public function __construct(EntityManagerInterface $em, VinRepository $vin)
     {
         $this->em   = $em;
@@ -36,6 +41,17 @@ class CaveAVinController extends AbstractController
             "blanc" => 0,
             "rouge" => 0,
             "rose"  => 0
+        );
+        $this->region = array
+        (
+            "alsace" => 0,
+            "bordeaux" => 0,
+            "bourgogne" => 0,
+            "cote_rhone" => 0,
+            "corse" => 0,
+            "languedoc" => 0,
+            "loire" => 0,
+            "etranger" => 0
         );
     }
 
@@ -53,7 +69,7 @@ class CaveAVinController extends AbstractController
 
         $vins = $qb->getQuery();
 
-        // Count
+        // Count couleur
         foreach ($this->couleur as $couleur => $nombre)
         {
             $this->couleur[$couleur] = $this->vin->createQueryBuilder('v')
@@ -66,9 +82,23 @@ class CaveAVinController extends AbstractController
                 ->getSingleScalarResult();
         }
 
+        // Count region
+        foreach ($this->region as $region => $nombre)
+        {
+            $this->region[$region] = $this->vin->createQueryBuilder('v')
+                ->select('count(v.id)')
+                ->where('v.region = :region')
+                ->andWhere('v.quantite > :quantite')
+                ->setParameter('region', $region)
+                ->setParameter('quantite', 0)
+                ->getQuery()
+                ->getSingleScalarResult();
+        }
+
         return $this->render("cave/macave.html.twig", [
-            'vins' => $vins->getResult(),
-            'couleurs' => $this->couleur
+            'vins'      => $vins->getResult(),
+            'couleurs'  => $this->couleur,
+            'regions'   => $this->region
         ]);
     }
 
@@ -105,6 +135,76 @@ class CaveAVinController extends AbstractController
         
         return $this->render("cave/archive.html.twig", [
             'vins' => $vins->getResult()
+        ]);
+    }
+
+    /**
+     * @Route("/caveavin/filtre/{filtre}", name="caveavin.filtre.couleur")
+     * @return Response
+     */
+    public function filtreVin ($filtre): Response
+    {
+        $vins = null;
+
+        if (array_key_exists($filtre, $this->couleur))
+        {
+            $qb = $this->vin->createQueryBuilder('v')
+                ->where('v.couleur = :couleur')
+                ->andWhere('v.quantite > :quantite')
+                ->setParameter('couleur', $filtre)
+                ->setParameter('quantite', 0)
+                ->orderBy('v.annee', 'ASC');
+
+            $vins = $qb->getQuery();
+
+            // Count region
+            foreach ($this->region as $region => $nombre)
+            {
+                $this->region[$region] = $this->vin->createQueryBuilder('v')
+                    ->select('count(v.id)')
+                    ->where('v.region = :region')
+                    ->andWhere('v.quantite > :quantite')
+                    ->andWhere('v.couleur = :couleur')
+                    ->setParameter('region', $region)
+                    ->setParameter('quantite', 0)
+                    ->setParameter('couleur', $filtre)
+                    ->getQuery()
+                    ->getSingleScalarResult();
+            }
+        }
+        else if (array_key_exists($filtre, $this->region))
+        {
+            $qb = $this->vin->createQueryBuilder('v')
+                ->where('v.region = :region')
+                ->andWhere('v.quantite > :quantite')
+                ->setParameter('region', $filtre)
+                ->setParameter('quantite', 0)
+                ->orderBy('v.annee', 'ASC');
+
+            $vins = $qb->getQuery();
+
+            // Count couleur
+            foreach ($this->couleur as $couleur => $nombre)
+            {
+                $this->couleur[$couleur] = $this->vin->createQueryBuilder('v')
+                    ->select('count(v.id)')
+                    ->where('v.couleur = :couleur')
+                    ->andWhere('v.quantite > :quantite')
+                    ->andWhere('v.region = :region')
+                    ->setParameter('couleur', $couleur)
+                    ->setParameter('quantite', 0)
+                    ->setParameter('region', $filtre)
+                    ->getQuery()
+                    ->getSingleScalarResult();
+            }
+        }
+
+        return $this->json(
+        [
+            'vins'      => $vins->getResult(),
+            'couleurs'  => $this->couleur,
+            'regions'   => $this->region,
+            'filtres'   => $filtre
         ]);
     }
 
