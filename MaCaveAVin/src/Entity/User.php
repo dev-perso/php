4b182e847636b9a26c4cd42e49e808bd4a259a1c
Michael Paris <paris.michael@hotmail.fr>
2020-02-26 00:34:37 +0100

    evol

--- MaCaveAVin/src/Entity/User.php
+++ MaCaveAVin/src/Entity/User.php
@@ -131,7 +131,7 @@ class User implements UserInterface
      */
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     /**
