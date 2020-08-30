13662555c0c412c291fe2af52ecec11eb611d052
Michael Paris <michael.paris@nrx.eu>
2020-08-30 18:35:29 +0200

    evol auth

--- MaCaveAVin/public/js/connexion.js
+++ MaCaveAVin/public/js/connexion.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function()
 {
     var passwordInput           = document.getElementById("passwordInput");
     var passwordRequirements    = document.getElementById("requirements");
+    var lengthRequirement       = document.getElementById('passLength');
+    var minRequirement          = document.getElementById('passMin');
+    var majRequirement          = document.getElementById('passMaj');
+    var numRequirement          = document.getElementById('passNum');
+
+    var registerBtn             = document.getElementById('registerBtn');
     var register                = document.getElementById("register");
     var connect                 = document.getElementById("connect");
     var signUpForm              = document.getElementById("signUpForm");
@@ -35,51 +41,101 @@ document.addEventListener("DOMContentLoaded", function()
 
     passwordInput.addEventListener('blur', e =>
     {
-        e.target.style.background = '';
         passwordRequirements.style.display = 'none';
     });
 
     passwordInput.addEventListener("keyup", e =>
     {
         var password = passwordInput.value;
+        isStrongPwd2(password);
     });
 
 
     function isStrongPwd2(password)
     {
-        var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const regexMaj  = RegExp('[A-Z]');
+        const regexMin  = RegExp('[a-z]');
+        const regexNum  = RegExp('[0-9]');
 
-        var lowercase = "abcdefghijklmnopqrstuvwxyz";
+        // Si le password contient au moins une Majuscule
+        var uppercaseFlag = regexMaj.test(password);
+        // Si le password contient au moins une Minuscule
+        var lowcaseFlag = regexMin.test(password);
+        // Si le password contient au moins un Chiffre
+        var numbercaseFlag = regexNum.test(password);
 
-        var digits = "0123456789";
-
-        var splChars ="!@#$%&*()";
-
-        var ucaseFlag = contains(password, uppercase);
-
-        var lcaseFlag = contains(password, lowercase);
-
-        var digitsFlag = contains(password, digits);
-
-        var splCharsFlag = contains(password, splChars);
-
-        if(password.length>=8 && ucaseFlag && lcaseFlag && digitsFlag && splCharsFlag)
-            return true;
+        if (uppercaseFlag)
+        {
+            majRequirement.querySelector('.fa-times').style.display = 'none';
+            majRequirement.querySelector('.fa-check').style.display = 'inline';
+            majRequirement.style.color = '#155724';
+        }
         else
-            return false;
+        {
+            majRequirement.querySelector('.fa-times').style.display = 'inline';
+            majRequirement.querySelector('.fa-check').style.display = 'none';
+            majRequirement.style.color = '#721c24';
+        }
+
+        if (lowcaseFlag)
+        {
+            minRequirement.querySelector('.fa-times').style.display = 'none';
+            minRequirement.querySelector('.fa-check').style.display = 'inline';
+            minRequirement.style.color = '#155724';
+        }
+        else
+        {
+            minRequirement.querySelector('.fa-times').style.display = 'inline';
+            minRequirement.querySelector('.fa-check').style.display = 'none';
+            minRequirement.style.color = '#721c24';
+        }
+
+        if (numbercaseFlag)
+        {
+            numRequirement.querySelector('.fa-times').style.display = 'none';
+            numRequirement.querySelector('.fa-check').style.display = 'inline';
+            numRequirement.style.color = '#155724';
+        }
+        else
+        {
+            numRequirement.querySelector('.fa-times').style.display = 'inline';
+            numRequirement.querySelector('.fa-check').style.display = 'none';
+            numRequirement.style.color = '#721c24';
+        }
+
+        if (password.length>=8)
+        {
+            lengthRequirement.querySelector('.fa-times').style.display = 'none';
+            lengthRequirement.querySelector('.fa-check').style.display = 'inline';
+            lengthRequirement.style.color = '#155724';
+        }
+        else
+        {
+            lengthRequirement.querySelector('.fa-times').style.display = 'inline';
+            lengthRequirement.querySelector('.fa-check').style.display = 'none';
+            lengthRequirement.style.color = '#721c24';
+        }
+
+        if(password.length>=8 && uppercaseFlag && lowcaseFlag && numbercaseFlag)
+        {
+            passwordRequirements.querySelector('.alert-danger').style.background = '#c3e6cb';
+            registerBtn.disabled = false;
+        }
+        else
+        {
+            passwordRequirements.querySelector('.alert-danger').style.background = '#f8d7da';
+            registerBtn.disabled = true;
+        }
 
     }
 
-    function contains(password, allowedChars) {
-
-        for (i = 0; i < password.length; i++) {
-
+    function contains(password, allowedChars)
+    {
+        for (i = 0; i < password.length; i++)
+        {
             var char = password.charAt(i);
-
             if (allowedChars.indexOf(char) >= 0) { return true; }
-
         }
-
         return false;
     }
 
