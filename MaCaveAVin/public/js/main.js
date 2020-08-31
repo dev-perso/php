ac36b98c5910664aa0a64e2b614cb1bce6a0cdf8
MParisNRX <michael.paris@nrx.eu>
2020-08-31 16:59:02 +0200

    suite

---
+++ MaCaveAVin/public/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+    var cave        = document.getElementById("caveBtn");
+    var profile     = document.getElementById("profileBtn");
+    var archive     = document.getElementById("archiveBtn");
+}
